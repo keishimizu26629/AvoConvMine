bacb3a55d4bcdb5920ed3e4ca870ddf01269eea3
keishimizu26629 <keisukeshimizu.inoworl@gmail.com>
2024-08-17 02:02:28 +0900

    Fix import issues and project structure

--- api/app/routes/__init__.py
+++ api/app/routes/__init__.py
@@ -1,1 +1,1 @@
-from app.routes import user_routes, conversation_routes, friend_routes, chat_routes, auth_routes, test_routes
+from . import user_routes, conversation_routes, friend_routes, chat_routes, auth_routes, test_routes
