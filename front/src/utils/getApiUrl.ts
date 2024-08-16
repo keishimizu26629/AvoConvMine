f778885071ddd8546324441c9d5a3c4a39384ac6
keishimizu26629 <keisukeshimizu.inoworl@gmail.com>
2024-08-17 07:36:05 +0900

    fix url

--- front/src/utils/getApiUrl.ts
+++ front/src/utils/getApiUrl.ts
@@ -1,4 +1,4 @@
 export const getApiUrl = (): string => {
-  return 'https://avo-conv-mine-6c40b00157f0.herokuapp.com';
+  return 'https://avo-conv-mine-6c40b00157f0.herokuapp.com'; // 本番環境
   // return 'http://localhost:8000';
 };
